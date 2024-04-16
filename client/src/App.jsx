0f3e6f77b2faa7c2b51e6687b039e9c21a2ced39
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-04-16 18:07:34 +0530

    Created Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Signin from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 function App() {
   return (
   <BrowserRouter>
+  <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
