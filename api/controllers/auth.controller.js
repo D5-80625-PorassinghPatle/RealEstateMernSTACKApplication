bd787fd17cf97112141f9d93a406b446cb855dc1
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-04-18 15:58:34 +0530

    Signup Page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
 
-export const signup= async (req,res) =>{
+export const signup= async (req,res,next) =>{
 
 const {username,email,password} =req.body;
 
@@ -16,7 +16,7 @@ res.status(201).json("User created sucessfully");
     
 } catch (error) {
 
-    res.status(500).json(error.message)
-    
+
+    next(error);
 }
 };
---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+export const errorHandler =(statusCode, message) =>{
+
+    const error = new Error(message);//js error constructor 
+    error.statusCode = statusCode ;
+    error.message =message;
+    return error;
+}
