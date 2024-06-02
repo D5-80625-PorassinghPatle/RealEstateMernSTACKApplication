934a96bc1a8591cdbd6012544737c85444e4bd0f
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-06-02 13:01:21 +0530

    "Added user listing feature, updated create listing page, and made changes to profile page"

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { test } from "../controllers/user.controller.js";
-import { updateUser } from "../controllers/user.controller.js";
+import { updateUser,getUserListings } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { deleteUser } from "../controllers/user.controller.js";
 
@@ -11,6 +11,8 @@ const router = express.Router();
 router.get("/test",test);
 router.post("/update/:id" ,verifyToken , updateUser);
 router.delete("/delete/:id" ,verifyToken , deleteUser);
+router.get("/listings/:id" ,verifyToken , getUserListings);
+
 
 
 
