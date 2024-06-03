c57c17033944b517955476fe6afe131b68a9babb
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-06-03 14:04:50 +0530

    "Added static file serving and client-side routing to API, updated package.json scripts for Deployment"

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
 
 dotenv.config();
+import path from "path";
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,6 +18,8 @@ mongoose
     console.log(err);
   });
 
+
+  const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -31,6 +34,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 //configuring middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
