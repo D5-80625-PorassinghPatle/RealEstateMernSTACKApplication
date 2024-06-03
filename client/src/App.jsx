9314a90e9d8a6af96fe6a75989b8b81b9f8fc5db
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-06-03 11:03:11 +0530

    "Added swiper dependency and updated package-lock.json files"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,15 +9,19 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+
+        <Route path='/listing/:listingId' element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
