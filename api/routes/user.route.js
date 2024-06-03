be2793bf7c8491077baaacc0303dae7973f2ed65
D5-80625-PorassinghPatle <poraspatle@gmail.com>
2024-06-03 11:06:18 +0530

    "Added getUser endpoint to user controller and route, and updated Listing component to display contact button and form"

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { test } from "../controllers/user.controller.js";
-import { updateUser,getUserListings } from "../controllers/user.controller.js";
+import { updateUser,getUserListings,getUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { deleteUser } from "../controllers/user.controller.js";
 
@@ -12,6 +12,8 @@ router.get("/test",test);
 router.post("/update/:id" ,verifyToken , updateUser);
 router.delete("/delete/:id" ,verifyToken , deleteUser);
 router.get("/listings/:id" ,verifyToken , getUserListings);
+router.get('/:id', verifyToken, getUser)
+
 
 
 
